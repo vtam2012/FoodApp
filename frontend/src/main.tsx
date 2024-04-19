2aa586322f3e64be8578a9b8f84bafdba54a99e0
Vincent Tam <vtam2012@gmail.com>
2024-04-19 15:15:59 -0700

    Finished adding NavBars

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import './index.css'
+import './globals.css'
 import { BrowserRouter as Router } from 'react-router-dom'
 import AppRoutes from './AppRoutes'
 
