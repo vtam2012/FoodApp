9bb1baea7ad699783e4d3a0c46e47d74005a0401
Vincent Tam <vtam2012@gmail.com>
2024-04-18 17:36:57 -0700

    Added routing for frontend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,6 +2,9 @@ import express from 'express'
 import { Request, Response } from 'express'
 import cors from 'cors'
 import "dotenv/config"
+import mongoose from 'mongoose'
+
+mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string).then(() => console.log("Connected to database!"))
 
 const app = express()
 app.use(express.json())
